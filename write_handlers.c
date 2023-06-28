c9a72889fa44d030fda2b5a13ff4e9199d2e9f55
Tolulope <tolugbenga72@gmail.com>
2023-06-27 19:42:02 -0700

    man

--- write_handlers.c
+++ write_handlers.c
@@ -14,35 +14,17 @@
 int handle_write_char(char c, char buffer[],
 	int flags, int width, int precision, int size)
 {
-	 /* char is stored at left and paddind at buffer's right */
-	int i = 0;
-	char padd = ' ';
+	 /* char is stored at left and padd
+*/
 
-	UNUSED(precision);
-	UNUSED(size);
-
-	if (flags & F_ZERO)
-		padd = '0';
-
-	buffer[i++] = c;
-	buffer[i] = '\0';
-
-	if (width > 1)
-	{
-		buffer[BUFF_SIZE - 1] = '\0';
-		for (i = 0; i < width - 1; i++)
-			buffer[BUFF_SIZE - i - 2] = padd;
-
-		if (flags & F_MINUS)
-			return (write(1, &buffer[0], 1) +
-					write(1, &buffer[BUFF_SIZE - i - 1], width - 1));
-		else
-			return (write(1, &buffer[BUFF_SIZE - i - 1], width - 1) +
-					write(1, &buffer[0], 1));
-	}
-	return (write(1, &buffer[0], 1));
+void print_buffer(char buffer[], int *buff_ind)
+{
+        if (*buff_ind > 0)
+        {
+                write(1, &buffer[0], *buff_ind);
+                *buff_ind = 0;
+        }
 }
-
 /**
  * write_number - Prints a string
  * @is_negative: Lista of arguments
